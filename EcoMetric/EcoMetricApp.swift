a3d1c4f91ea33ea0ee7bf7625304e01c2298cf37
@NXThanh01 <nguyenxuanthanh010307fptedu@gmail.com>
2026-09-19 00:08:54 +0700

    Initial Commit

---
+++ EcoMetric/EcoMetricApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoMetricApp.swift
+//  EcoMetric
+//
+//  Created by Nguyễn Xuân Thành on 19/9/26.
+//
+
+import SwiftUI
+
+@main
+struct EcoMetricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcoMetricTests/EcoMetricTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcoMetricTests.swift
+//  EcoMetricTests
+//
+//  Created by Nguyễn Xuân Thành on 19/9/26.
+//
+
+import Testing
+@testable import EcoMetric
+
+struct EcoMetricTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
